9f4f87c1a412eb579fd125dcab473f9bd0688092
smilroy <shannon.milroy@mail.harvard.edu>
2022-08-27 14:54:41 -0400

    initial push to gh

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class = "navbar" role = "navigation">
+    <div class = "navbar-menu">
+      <a class = "navbar-item">
+        <router-link to="/">Home</router-link>
+      </a>
+
+      <a class = "navbar-item">
+        <router-link to="/about">About</router-link>
+      </a>
+    </div>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+} */
 </style>
