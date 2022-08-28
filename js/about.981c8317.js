18d7ff2182be883f7142f07327e3525b463608c5
smilroy <shannon.milroy@mail.harvard.edu>
2022-08-28 12:54:16 -0400

    updating gh-pages

--- js/about.981c8317.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkwebpage"]=self["webpackChunkwebpage"]||[]).push([[443],{7381:function(e,n,a){a.r(n),a.d(n,{default:function(){return f}});var u=a(3396);const t={class:"about"},r=(0,u._)("h1",null,"This is an about page",-1),s=[r];function c(e,n){return(0,u.wg)(),(0,u.iD)("div",t,s)}var i=a(89);const o={},b=(0,i.Z)(o,[["render",c]]);var f=b}}]);
-//# sourceMappingURL=about.981c8317.js.map
